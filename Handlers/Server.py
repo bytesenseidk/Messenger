32c546e8e24d678f762bb9f12e8d032cfee2c997
Byte Sensei <50791042+bytesenseidk@users.noreply.github.com>
2023-12-30 12:47:41 +0100

    Server setup

---
+++ Handlers/Server.py
@@ -0,0 +1,36 @@
+import select
+from server_handler import ServerHandler
+
+class Server(object):
+    def __init__(self):
+        self.server = ServerHandler()
+    
+    def run(self):
+        while True:
+            read_sockets, _, exception_sockets = select.select(self.server.sockets_list, [], self.server.sockets_list)
+            for notified_socket in read_sockets:
+                if notified_socket == self.server.server_socket:
+                    client_socket, client_address = self.server.server_socket.accept()
+                    user = self.server.receive_message(client_socket)
+                    if user is False:
+                        continue
+                    self.server.sockets_list.append(client_socket)
+                    self.server.clients[client_socket] = user
+                    ip, port = client_address
+                    print(f"\n[ USER CONNECTED: {user['data'].decode('utf-8')} ]\n"
+                        f"[IP: {ip}]\n[Port: {port}]\n")
+                else:
+                    message = self.server.receive_message(notified_socket)
+                    if message is False:
+                        print(f"[ CONNECTION TERMINATED: {user['data'].decode('utf-8')} ]")
+                        self.server.sockets_list.remove(notified_socket)
+                        del self.server.clients[notified_socket]
+                        continue
+                    user = self.server.clients[notified_socket]
+                    print(f"{user['data'].decode('utf-8')}  >> {message['data'].decode('utf-8')}")
+                    for self.server.client_socket in self.server.clients:
+                        if self.server.client_socket != notified_socket:
+                            self.server.client_socket.send(user["header"] + user["data"] + message["header"] + message["data"])
+            for notified_socket in exception_sockets:
+                Server.sockets_list.remove(notified_socket)
+                del self.server.clients[notified_socket]
--- Handlers/server_handler.py
+++ Handlers/server_handler.py
@@ -1,8 +1,7 @@
 import os
-import select
 import socket
 
-class Server(object):
+class ServerHandler(object):
     def __init__(self, IP="127.0.0.1", PORT=1234):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -23,35 +22,3 @@ class Server(object):
             return {"header": message_header, "data": client_socket.recv(message_length)}
         except:
             return False
-
-if __name__ == "__main__":
-    server = Server()
-    while True:
-        read_sockets, _, exception_sockets = select.select(server.sockets_list, [], server.sockets_list)
-        for notified_socket in read_sockets:
-            if notified_socket == server.server_socket:
-                client_socket, client_address = server.server_socket.accept()
-                user = server.receive_message(client_socket)
-                if user is False:
-                    continue
-                server.sockets_list.append(client_socket)
-                server.clients[client_socket] = user
-                ip, port = client_address
-                print(f"\n[ USER CONNECTED: {user['data'].decode('utf-8')} ]\n"
-                      f"[IP: {ip}]\n[Port: {port}]\n")
-            else:
-                message = server.receive_message(notified_socket)
-                if message is False:
-                    print(f"[ CONNECTION TERMINATED: {user['data'].decode('utf-8')} ]")
-                    server.sockets_list.remove(notified_socket)
-                    del server.clients[notified_socket]
-                    continue
-                user = server.clients[notified_socket]
-                print(f"{user['data'].decode('utf-8')}  >> {message['data'].decode('utf-8')}")
-                for server.client_socket in server.clients:
-                    if server.client_socket != notified_socket:
-                        server.client_socket.send(user["header"] + user["data"] + message["header"] + message["data"])
-        for notified_socket in exception_sockets:
-            Server.sockets_list.remove(notified_socket)
-            del server.clients[notified_socket]
-
